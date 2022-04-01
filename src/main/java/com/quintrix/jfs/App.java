6dfb66df30c0223d61a60be63b61954c07c0fd39
Hamilton Laws <hamiltonlaws233@gmail.com>
2022-04-01 19:26:51 -0400

    Crated the getter and setter for Class Dog

--- src/main/java/com/quintrix/jfs/App.java
+++ src/main/java/com/quintrix/jfs/App.java
@@ -5,10 +5,10 @@ public class App
 {
     public static void main( String[] args )
     {
-       Dog dogOne = new Dog();
+       Dog dogOne = new Dog("Choppo",3,"Chiwawa",(float) 40.66);
        
-       dogOne.setDog("Choppo",3,"Chiwawa",(float) 40.66);
+       System.out.println(dogOne.toString());
        
-       System.out.println("The name of the dog is " + dogOne.getAge() + "\nThey are " + dogOne.getAge() + "\nThey are a "+ dogOne.getBread()+ "\nThey are for sale for $" + dogOne.getPrice());
+       System.out.println("The name of the dog is " + dogOne.getAge() + "\nThey are " + dogOne.getAge() + "\nThey are a "+ dogOne.getBreed()+ "\nThey are for sale for $" + dogOne.getPrice());
     }
 }
--- src/main/java/com/quintrix/jfs/Dog.java
+++ src/main/java/com/quintrix/jfs/Dog.java
@@ -4,27 +4,48 @@ public class Dog {
 	
 	private String name;
 	private int age;
-	private String bread;
+	private String breed;
 	private float price;
 	
-	public void setDog(String newName,int newAge, String newBread, float newPrice) {
-		this.name = newName;
-		this.age = newAge;
-		this.bread = newBread;
-		this.price = newPrice;
+	
+	public Dog(String name, int age, String breed, float price) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.breed = breed;
+		this.price = price;
 	}
 	
+	@Override
+	public String toString() {
+		return "Dog [name=" + name + ", age=" + age + ", breed=" + breed + ", price=" + price + "]";
+	}
+
 	public String getName() {
 		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
 	}
 	public int getAge() {
 		return age;
 	}
-	public String getBread() {
-		return bread;
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getBreed() {
+		return breed;
+	}
+	public void setBreed(String breed) {
+		this.breed = breed;
 	}
 	public float getPrice() {
 		return price;
 	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+
 
 }
