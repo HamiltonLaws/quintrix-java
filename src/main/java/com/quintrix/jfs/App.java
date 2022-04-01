e0ace0624a5b33a39231643b720e3aa2376160cc
Hamilton Laws <hamiltonlaws233@gmail.com>
2022-04-01 18:13:53 -0400

    fitst comit

---
+++ src/main/java/com/quintrix/jfs/App.java
@@ -0,0 +1,15 @@
+package com.quintrix.jfs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       for(int i =0; i<8; i++) {
+    	   System.out.println("Hello " + i);
+       }
+    }
+}
