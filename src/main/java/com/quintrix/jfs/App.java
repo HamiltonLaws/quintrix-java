f542fcce184bd272a2a690228dc092aba3cf7864
Hamilton Laws <hamiltonlaws233@gmail.com>
2022-04-01 20:32:45 -0400

    Added the ArrayList

--- src/main/java/com/quintrix/jfs/App.java
+++ src/main/java/com/quintrix/jfs/App.java
@@ -1,5 +1,7 @@
 package com.quintrix.jfs;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class App 
 {
@@ -7,7 +9,18 @@ public class App
     {
        Dog dogOne = new Dog("Choppo",3,"Chiwawa",(float) 40.66);
        
-       System.out.println(dogOne.toString());
+       
+       List<Dog> dogList = new ArrayList<>();
+       
+       dogList.add(dogOne);
+       
+       Dog dogTwo = new Dog("Jasper", 6, "Husky", (float) 100.99);
+       
+       dogList.add(dogTwo);
+       
+       for(Dog d : dogList) {
+    	   System.out.println(d.toString());
+       }
        
        System.out.println("The name of the dog is " + dogOne.getAge() + "\nThey are " + dogOne.getAge() + "\nThey are a "+ dogOne.getBreed()+ "\nThey are for sale for $" + dogOne.getPrice());
     }
