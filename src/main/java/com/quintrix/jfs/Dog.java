932956cec1a48f79065e58325638bbc5737bd26e
Hamilton Laws <hamiltonlaws233@gmail.com>
2022-04-05 20:36:31 -0400

    Commit after more changes

--- src/main/java/com/quintrix/jfs/Dog.java
+++ src/main/java/com/quintrix/jfs/Dog.java
@@ -1,13 +1,13 @@
 package com.quintrix.jfs;
 
 public class Dog {
-	
+
 	private String name;
 	private int age;
 	private String breed;
 	private float price;
-	
-	
+
+	// creation
 	public Dog(String name, int age, String breed, float price) {
 		super();
 		this.name = name;
@@ -15,7 +15,7 @@ public class Dog {
 		this.breed = breed;
 		this.price = price;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Dog [name=" + name + ", age=" + age + ", breed=" + breed + ", price=" + price + "]";
@@ -24,28 +24,33 @@ public class Dog {
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public String getBreed() {
 		return breed;
 	}
+
 	public void setBreed(String breed) {
 		this.breed = breed;
 	}
+
 	public float getPrice() {
 		return price;
 	}
+
 	public void setPrice(float price) {
 		this.price = price;
 	}
-	
-
 
 }
---
+++ src/main/java/com/quintrix/jfs/JASONParser.java
@@ -0,0 +1,8 @@
+package com.quintrix.jfs;
+
+public class JASONParser {
+
+	public static void main(String[] args) {
+
+	}
+}
